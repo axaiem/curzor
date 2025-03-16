26ce2f7703332a1b59899bf7a62a126e6718de1a
axaiem <akshaymcode@gmail.com>
2025-03-16 18:34:08 +0530

    main updated to created an simple empty window

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
-int main(){
+#include <QApplication>
+#include <QWidget>
+int main(int argc, char *argv[]){
     std::cout << "Welcome to Curzor!" << std::endl;
+    QApplication app(argc, argv);
+    QWidget window;
+    window.setWindowTitle("Curzor");
+    window.resize(800, 600);
+    window.show();
+    return app.exec();
 }
