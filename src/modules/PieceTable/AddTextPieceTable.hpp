339a8381ea80fb9c0692fd7b55eb4231b7843497
axaiem <akshaymcode@gmail.com>
2025-04-27 17:31:32 +0530

    first implimentation of piece table and other interfaces

---
+++ src/modules/PieceTable/AddTextPieceTable.hpp
@@ -0,0 +1,28 @@
+#include "../AllOperations/Operation.hpp"
+#include "PieceTable.hpp"
+#include "DllPieces.hpp"
+#ifndef ADDTEXTPIECETABLE_HPP
+#define ADDTEXTPIECETABLE_HPP
+
+class AddTextPieceTable: public Operation{
+    private:
+        DllPieces& m_tablePiece;
+        int m_idx;
+        std::string m_changesBuffer;
+
+    public:
+
+    AddTextPieceTable(DllPieces& tablePiece, int idx, const std::string& text , std::string& change): m_tablePiece (tablePiece), m_idx (idx), m_changesBuffer(change) {
+    }
+
+    void run(){
+        // to be defined
+    }
+
+    void undo(){
+        // impliment undo operations
+    }
+
+};
+
+#endif
---
+++ src/modules/PieceTable/Pieces.hpp
@@ -0,0 +1,9 @@
+#ifndef PIECES_HPP
+#define PIECES_HPP
+
+
+class Pieces{
+    public:
+};
+
+#endif
